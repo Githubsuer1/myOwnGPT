840e01cf1db02f7d452d7f941d21ad3a39e38d61
Githubsuer1 <mishrasiddhant560@gmail.com>
2025-02-08 23:58:47 +0530

    app.jsx updated

--- server/routes/client.routes.js
+++ server/routes/client.routes.js
@@ -5,7 +5,7 @@ import { handleGroqAPI, getResponse } from '../controllers/client.controllers.js
 const app = express()
 const router = express.Router();
 
-router.route('/user').post(handleGroqAPI);
+router.route('/').post(handleGroqAPI);
 router.route('/').get(getResponse);
 
 
